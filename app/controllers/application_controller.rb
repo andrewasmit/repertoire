76ddf41226b50727e70062db8e411fd489862b61
Andrew <andrewasmit@hotmail.com>
2023-02-13 15:16:00 -0600

    added a few GET routes in the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,21 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Hello, World!" }.to_json
+  get "/library" do
+    pieces = Piece.all
+    pieces.to_json(include: :notes)
   end
 
+  get "/library/:id" do
+    Piece.find(params[:id]).to_json(include: :notes)
+  end
+
+
+  get "/concerts" do
+    concerts = Concert.all
+    concerts.to_json(include: { pieces: { include: :notes } })
+  end
+
+
+# End of ApplicationController
 end
