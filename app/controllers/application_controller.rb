07d16b4f6659ee9f4d45578fdab221f4dc00b25b
andrew <andrewasmit@hotmail.com>
2023-02-14 21:38:44 -0600

    updated the API get routes for /library and /concerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,22 @@ class ApplicationController < Sinatra::Base
     pieces.to_json(include: :notes)
   end
 
-  get "/library/:id" do
-    Piece.find(params[:id]).to_json(include: :notes)
-  end
+  # get "/library/:id" do
+  #   Piece.find(params[:id]).to_json(include: :notes)
+  # end
 
 
   get "/concerts" do
     concerts = Concert.all
-    concerts.to_json(include: { pieces: { include: :notes } })
+    concerts.to_json(include: [:performances, :ensembles])
   end
 
-  get "/concerts/:id" do
-    concert = Concert.find(params[:id])
-    concert.to_json(include: :pieces)
-  end
+  # get "/concerts/:id" do
+  #   concert = Concert.find(params[:id])
+  #   concert.to_json(include: :pieces)
+  # end
 
 
+  
 # End of ApplicationController
 end
