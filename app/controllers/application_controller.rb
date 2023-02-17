d61d9a25d1c147b74fe2c4bf1fdcd64dce922793
andrew <andrewasmit@hotmail.com>
2023-02-16 20:27:21 -0600

    full crud for Piece. Not yet for notes. Full CRUD for COncert. Not yet performances or ensembles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,79 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  #CRUD for Pieces
   get "/library" do
     pieces = Piece.all
     pieces.to_json(include: :notes)
   end
 
-  # get "/library/:id" do
-  #   Piece.find(params[:id]).to_json(include: :notes)
-  # end
+  get "/library/:id" do
+    piece = Piece.find(params[:id])
+    piece.to_json(include: :notes)
+  end
+
+  delete "/library/:id" do
+    piece = Piece.find(params[:id])
+    piece.destroy 
+    piece.to_json
+  end
+
+  post "/library" do
+    new_piece = Piece.create(
+      title: params[:title],
+      composer: params[:composer],
+      number_of_players: params[:number_of_players],
+      genre: params[:genre],
+      difficulty: params[:difficulty],
+      arranger: params[:arranger],
+      reference_recording: params[:reference_recording],
+    )
+    new_piece.to_json
+  end
+
+  patch "/library/:id" do
+    piece = Piece.find(params[:id])
+    piece.update(
+      reference_recording: params[:reference_recording],
+      composer: params[:composer],
+      arranger: params[:arranger],
+      title: params[:title],
+      number_of_players: params[:number_of_players],
+      genre: params[:genre],
+      difficulty: params[:difficulty]
+    )
+    piece.to_json
+  end
 
 
+  # CRUD for Concerts
   get "/concerts" do
     concerts = Concert.all
     concerts.to_json(include: [:performances, :ensembles])
   end
 
-  # get "/concerts/:id" do
-  #   concert = Concert.find(params[:id])
-  #   concert.to_json(include: :pieces)
-  # end
+  delete "/concerts/:id" do
+    concert = Concert.find(params[:id])
+    concert.destroy
+    concert.to_json
+  end
+
+  post "/concerts" do
+    new_concert = Concert.create(
+      concert_description: params[:concert_description],
+      year: params[:year]
+    )
+    new_concert.to_json
+  end
+
+  patch "/concerts/:id" do
+    concert = Concert.find(params[:id])
+    concert.update(
+      concert_description: params[:concert_description],
+      year: params[:year]
+    )
+    concert.to_json
+  end
 
 
   
